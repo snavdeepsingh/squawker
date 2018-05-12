89aba9ca4199cd3e35b6bf8f5316aa12b9e17af1
snavdeepsingh <snavdeepsingh@gmail.com>
2018-05-12 12:49:25 -0500

    added passport test folder

---
+++ passport-oauth-test/config/passport-setup.js
@@ -0,0 +1,14 @@
+var passport = require("passport");
+var GoogleStrategy = require("passport-google-oauth20");
+var keys = require("./keys.js");
+
+passport.use(new GoogleStrategy({
+    // options for the google strategy object
+    callbackURL: "/auth/google/redirect",
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret
+}, function(accessToken, refreshToken, profile, done){
+    // passport callback function
+    console.log("passport callback fired");
+    console.log(profile);
+}))
---
+++ passport-oauth-test/routes/auth-routes.js
@@ -0,0 +1,30 @@
+var router = require("express").Router();
+var passport = require("passport");
+
+// auth login
+
+router.get("/login", function(req, res){
+    res.render("login");
+});
+
+// auth logout
+
+router.get("/logout", function(req, res){
+    // handle with passport
+    res.send("logging out");
+})
+
+// auth with google
+router.get("/google", passport.authenticate("google", {
+     // handle with passport
+     scope: ["profile"]
+}));
+
+// callback route for google to redirect to 
+router.get("/google/redirect", passport.authenticate("google") ,function(req, res){
+    res.send("You are here.")
+})
+
+module.exports = router;
+
+
