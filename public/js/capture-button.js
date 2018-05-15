2290982c2243e27f87110c6e07a9b6b4e5db7330
snavdeepsingh <snavdeepsingh@gmail.com>
2018-05-15 00:01:25 -0500

    added input label to select and upload, google-storage-routes.js, multer, and dev dependencies bebel-cli, babel-preset-es2015, babel-preset-stage-2

--- public/js/capture-button.js
+++ public/js/capture-button.js
@@ -15,14 +15,27 @@ $(function() {
         console.log(file);
     
         if (file !== null) {
-            console.log("-URL.createObjectURL(file)-");            
-            console.log(URL.createObjectURL(file));
+            // console.log("-URL.createObjectURL(file)-");            
+            // console.log(URL.createObjectURL(file));
+        //    var fileURL = URL.createObjectURL(file);
+            fetch("https://storage.googleapis.com/snavdeepsingh/", {
+                method: "POST",
+                body: file
+            }).then(
+                response => response.json() // if the response is a JSON object
+              ).then(
+                success => console.log(success) // Handle the success response object
+              ).catch(
+                error => console.log(error) // Handle the error response object
+              );
 
             // post pic to: <img id="output" />
             // $("#output")
             //     .attr("src", URL.createObjectURL(file))
             //     .width("150px")
             //     .height("150px");
+            // console.log(fileURL);
+            // return fileURL;
         }
     // END OF: function postImageToPage(fileList) {
     };
@@ -42,5 +55,3 @@ $(function() {
 
 // END OF: $(function() {
 });
-
-
