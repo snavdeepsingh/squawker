5ce2a1410e5015d56adc68bbd11aa51d22e206aa
Malazanian <irutner22@gmail.com>
2018-05-15 20:05:44 -0500

    add test json results

--- routes/vision-api.js
+++ routes/vision-api.js
@@ -10,12 +10,14 @@ function base64_encode(file) {
 }
 
 let testImage = '../public/images/harrishawk.jpg'
-let testURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Harris%27s_Hawk_%28Parabuteo_unicinctus%29_3_of_4_in_set.jpg/220px-Harris%27s_Hawk_%28Parabuteo_unicinctus%29_3_of_4_in_set.jpg"
+let testGoodURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Harris%27s_Hawk_%28Parabuteo_unicinctus%29_3_of_4_in_set.jpg/220px-Harris%27s_Hawk_%28Parabuteo_unicinctus%29_3_of_4_in_set.jpg"
+let testMediocreURL = "http://www.gretchengarner.com/images/color_landscapes/minnesota/pop_Birds_Nest_Minnesota_1.jpg"
+let testBogusURL = "https://images.pier1.com/dis/dw/image/v2/AAID_PRD/on/demandware.static/-/Sites-pier1_master/default/dw03df652e/images/2691730/2691730_1.jpg?sw=1600&sh=1600&impolicy=Bypass"
 
 let base64str = base64_encode(testImage);
 const apiCall = 'https://vision.googleapis.com/v1/images:annotate?key=' + keys.googleVision.apiKey
 
-// Local Image
+//// Local Image
 // const reqObj = {
 //   "requests": [
 //     {
@@ -45,7 +47,7 @@ const reqObj = {
     {
       "image": {
         "source": {
-          "imageUri": testURL
+          "imageUri": testBogusURL
         }
       },
       "features": [
@@ -65,15 +67,21 @@ const reqObj = {
   ]
 }
 
-axios.post(apiCall, reqObj)
-  .then((response) => {
-    // console.log(response);
-    console.log(JSON.stringify(response.data.responses, undefined, 2));
-    // console.log(response.data.responses[0].labelAnnotations)
-    // console.log(response.data.responses[0].webDetection);
-    // console.log(response.data.responses[0].webDetection.visuallySimilarImages[0].url);
-    // base64str = response.data.responses[0].webDetection.visuallySimilarImages[0].url
-    // axios.post(apiCall, reqObj)
-}).catch((e) => {
+// let visionQuery = function (apiCall, reqObj) {
+  axios.post(apiCall, reqObj)
+    .then((response) => {
+      // console.log(response);
+      console.log(JSON.stringify(response.data.responses, undefined, 2));
+      // console.log(response.data.responses[0].labelAnnotations)
+      // console.log(response.data.responses[0].webDetection);
+      // console.log(response.data.responses[0].webDetection.visuallySimilarImages[0].url);
+      // base64str = response.data.responses[0].webDetection.visuallySimilarImages[0].url
+      // axios.post(apiCall, reqObj)
+  }).catch((e) => {
     console.log(e.response);
-});
+  });
+// }
+
+// visionQuery();
+
+// module.exports = visionQuery;
