4ecd2ae8dc95ab652cc06f8bbe2be6ca5e4f25b4
Matt McCarthy <mattrmc@gmail.com>
2018-05-22 21:47:05 -0500

    It's broken but it's awesome

--- models/user.js
+++ models/user.js
@@ -13,8 +13,6 @@ module.exports = function(sequelize, DataTypes) {
     },
     profileIMG: {
       type: DataTypes.STRING,
-      // allowNull: false,
-      // defaultValue: ''
     }
   });
 
--- routes/google-storage-routes.js
+++ routes/google-storage-routes.js
@@ -3,7 +3,9 @@ var express = require("express");
 var memoryStorage = multer.memoryStorage();
 var storage = require("@google-cloud/storage");
 var db = require('../models');
-const visionAPI = require('./vision-api')
+var Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const visionAPI = require('./vision-api');
 
 const googleCloudStorage = storage({
   projectId: "Bird",
@@ -20,6 +22,7 @@ const bucket = googleCloudStorage.bucket(BucketName);
 
 module.exports = function (app){
   app.post("/upload", upload.single("file"), (req, res, next) => {
+    // console.log(req)
     // console.log(req.user.dataValues.googleID);
     if (!req.file) {
       res.status(400).send("No file uploaded.");
@@ -50,28 +53,52 @@ module.exports = function (app){
         })
         .then(() => {
           visionAPI.reqObj.requests[0].image.source.imageUri = publicUrl
-          
+
           return visionAPI.visionQuery(visionAPI.apiCall, visionAPI.reqObj)
         }).then((visionQueryResults) => {
           let birdNames = [];
           visionQueryResults[0].labelAnnotations.forEach(labelAnnotation => {
             birdNames.push(labelAnnotation.description);
           })
-          if (!birdNames.includes('fauna') || !birdNames.includes('bird')) {
+          if (birdNames.includes('fauna') || birdNames.includes('bird')) {
+            if (visionQueryResults[0].webDetection.fullMatchingImages || visionQueryResults[0].webDetection.partialMatchingImages) {
+              let newBirdNames = [];
+              visionQueryResults[0].webDetection.webEntities.forEach(entity => {
+                if (entity.description)
+                  newBirdNames.push(entity.description)
+              })
+              console.log(newBirdNames)
+              db.BirdNameMaster.find({
+                where: {
+                  BirdName : {
+                    [Op.regexp] : `(${newBirdNames.join('|')})`
+                  }
+                }
+              }).then((data) => {
+                res.render("results", {
+                  name: req.user.dataValues.username,
+                  image: req.user.dataValues.profileIMG,
+                  lastPictureSrc: publicUrl,
+                  birdType: data.dataValues.BirdName,
+                  similarImage: (visionQueryResults[0].webDetection.fullMatchingImages ? visionQueryResults[0].webDetection.fullMatchingImages[0].url :visionQueryResults[0].webDetection.partialMatchingImages[0].url),
+                })
+                console.log(res)
+              })
+            } else if (visionQueryResults[0].webDetection.visuallySimilarImages) {
+              res.render("results", {
+                name: req.user.dataValues.username,
+                image: req.user.dataValues.profileIMG,
+                lastPictureSrc: publicUrl
+                // birdType: birdInfo
+                // similarImage: birdInfo
+              })
+            }
+          } else {
             res.render("tryAgain", {
               name: req.user.dataValues.username,
               image: req.user.dataValues.profileIMG,
               lastPictureSrc: publicUrl
             })
-          } else {
-            let handleBarsObj = {
-              name: req.user.dataValues.username,
-              image: req.user.dataValues.profileIMG,
-              lastPictureSrc: publicUrl,
-              birdType: visionQueryResults[0].webDetection.webEntities[0].description,
-              similarImage: visionQueryResults[0].webDetection.visuallySimilarImages[0].url
-            }
-            res.render("results", handleBarsObj)
           }
         }).catch(err => console.log(err))
       });
