64ff5a6704794104f46066d9e0dd4f1280cd4149
Matt McCarthy <mattrmc@gmail.com>
2018-05-23 17:47:20 -0500

    Logout functionality

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,28 +1,22 @@
 var passport = require("passport");
 
-// auth login
 module.exports = function(app) {
 	app.get("/login", function(req, res){
     res.render("login");
 	});
 
-	// auth logout
-
-	app.get("/logout", function(req, res){
-	    // handle with passport
-	    res.send("logging out");
+	app.get("/auth/logout", function(req, res){
+	    req.logout();
+			return res.redirect('/');
 	})
 
-	// auth with google
 	app.get("/auth/google", passport.authenticate("google", {
-	     // handle with passport
 	     scope: ["profile"]
 	}));
 
 	app.get('/auth/google/callback', 
 	  passport.authenticate('google'),
 	  (req, res) => {
-	  	// console.log(req.user.)
 	  	res.redirect('/home')
   });
 }
